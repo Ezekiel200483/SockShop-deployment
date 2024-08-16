1635aca2284dce0634380a70e3e0deb91903224d
Ezekiel200483 <olopade.ezekiel1998@gmail.com>
2024-08-16 10:21:11 +0100

    added variable

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,58 +1,49 @@
 variable "region" {
-  description = "The AWS region to create resources in"
+  description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-west-2"  # Change this to your desired region
-}
-
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "private_subnets" {
-  description = "List of private subnet CIDR blocks"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "public_subnets" {
-  description = "List of public subnet CIDR blocks"
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
-
-variable "cluster_version" {
-  description = "The Kubernetes version for the EKS cluster"
-  type        = string
-  default     = "1.23"
-}
-
-variable "node_instance_type" {
-  description = "The instance type for the EKS worker nodes"
-  type        = string
-  default     = "t3.medium"
-}
-
-variable "min_size" {
-  description = "The minimum number of worker nodes"
-  type        = number
-  default     = 1
-}
-
-variable "max_size" {
-  description = "The maximum number of worker nodes"
-  type        = number
-  default     = 3
-}
-
-variable "desired_size" {
-  description = "The desired number of worker nodes"
-  type        = number
-  default     = 2
 }
 
 variable "cluster_name" {
   description = "The name of the EKS cluster"
   type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "cluster_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for the EKS node group"
+  type        = string
+}
+
+variable "min_size" {
+  description = "Minimum size of the EKS node group"
+  type        = number
+}
+
+variable "max_size" {
+  description = "Maximum size of the EKS node group"
+  type        = number
+}
+
+variable "desired_size" {
+  description = "Desired size of the EKS node group"
+  type        = number
 }
